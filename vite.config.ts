efa2b596a9d09aad698e7fe84dbb7f5b594a64fa
Emmanuel Araujo <ema.araujo.ea@gmail.com>
2024-03-24 20:52:16 -0300

    new index

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://emma-devmusic.github.io/clinica-giuliani/'
+  // base: 'https://emma-devmusic.github.io/clinica-giuliani/'
 })
